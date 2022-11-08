daa16a7fdbf3b8282c4fe2e04efe8ac1e43c20ee
ericarthuang <ericarthuang2021@gmail.com>
2022-11-08 19:33:29 +0800

    commim in 2022/11/08

--- Django_Blog_Project/settings.py
+++ Django_Blog_Project/settings.py
@@ -160,4 +160,5 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
+# default AWS S3 for providing Media Files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
